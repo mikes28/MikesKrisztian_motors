13ef7f4a98e28920975eb9b02e1817a8b2d8640a
mikes28 <mikeskrisztian06@gmail.com>
2025-03-14 10:29:26 +0100

    ReadFromFile done

---
+++ Statisztika.cs
@@ -0,0 +1,27 @@
+﻿namespace motors;
+
+internal class Statisztika
+{
+    private List<Motor> motors = new List<Motor>();
+
+
+    public void ReadFromFile(string fileName)
+    {
+        string[] lines = File.ReadAllLines(fileName);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(';');
+            string brand = parts[0];
+            string name = parts[1];
+            int releaseYear = int.Parse(parts[2]);
+            double performance = double.Parse(parts[3]);
+            double priceInEur = double.Parse(parts[4]);
+            Motor motor = new Motor(brand, name, releaseYear, performance, priceInEur);
+            motors.Add(motor);
+        }
+    }
+
+   
+
+
+}
